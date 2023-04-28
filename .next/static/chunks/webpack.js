ed371b317cc19c6793a7ee1ae174c36c3c2ffc15
Vitor Butkus <vitorbutkus@gmail.com>
2023-04-28 19:07:02 -0300

    set #footer-container of claim-widget to display:none in globals

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8df53dc2840cd20d"; }
+/******/ 		__webpack_require__.h = function() { return "80ed89f8a21b0672"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Section6.jsx
+++ components/Section6.jsx
@@ -7,7 +7,7 @@ const listingId = '1045020912';
 export default function Section6() {
   return (
     <div className='w-screen bg-black' id='faucet'>
-      <div className='h-16 w-full bg-black/80 sticky top-0 left-0 px-10 sm:px-20 flex items-center tracking-[6px] text-sm sm:text-md z-[100]'>
+      <div className='h-20 w-full bg-black/80 sticky top-0 left-0 px-10 sm:px-20 flex items-center tracking-[6px] text-sm sm:text-md z-[100]'>
         FAUCET (2023), FROM PREPARED SCREEN SERIES (2012-)
       </div>
       {/* <div className='w-full  pt-20 pb-32 px-10 sm:px-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-8 sm:gap-6'> */}
@@ -15,7 +15,7 @@ export default function Section6() {
         {/* <div className='rounded-sm bg-slate-900 h-fit'>
           <Section6CardItem instance={listingId} />
         </div> */}
-        <div className='px-10 sm:px-10 pt-10 pb-36 claim-widget font-din'>
+        <div className='px-2 sm:px-10 pt-10 pb-20 claim-widget font-din'>
           {/* <a
             href='https://app.manifold.xyz/c/firstfaucet'
             target='_blank'
