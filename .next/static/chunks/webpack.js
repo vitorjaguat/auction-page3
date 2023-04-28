2fdb85dce2d3ea216d0f0e2916db9b9b026a4c86
Vitor Butkus <vitorbutkus@gmail.com>
2023-04-28 18:55:00 -0300

    changed globals.css for claim button, from position:fixed to position:sticky

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2963d12d0819dab0"; }
+/******/ 		__webpack_require__.h = function() { return "8df53dc2840cd20d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
