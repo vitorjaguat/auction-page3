27003493caec389afa6efa2db9aebdf71a953117
Vitor Butkus <vitorbutkus@gmail.com>
2023-04-28 22:01:24 -0300

    open sea button padding-y

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c9a9cafde7dc8582"; }
+/******/ 		__webpack_require__.h = function() { return "243e97922cce2cd6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
