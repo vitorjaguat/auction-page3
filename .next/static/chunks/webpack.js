c15807bcbcc518b2eada37e878485b89054681c5
Vitor Butkus <vitorbutkus@gmail.com>
2023-05-12 12:27:48 -0300

    add unoptimized in Vista

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "243e97922cce2cd6"; }
+/******/ 		__webpack_require__.h = function() { return "0a62c6751e1f83db"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Vista.jsx
+++ components/Vista.jsx
@@ -3,7 +3,12 @@ import Image from 'next/image';
 export default function Vista({ img }) {
   return (
     <div className='w-screen overflow-hidden h-fit max-h-[700px] flex items-center'>
-      <Image src={img} alt='Exhibition view' className='object-cover' />
+      <Image
+        src={img}
+        alt='Exhibition view'
+        className='object-cover'
+        unoptimized={true}
+      />
     </div>
   );
 }
