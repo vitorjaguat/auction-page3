4da8824cddbe177958e616c866845a46756a34d8
Vitor Butkus <vitorbutkus@gmail.com>
2026-08-23 19:28:34 -0300

    globals.css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "767f517f8db972e1"; }
+/******/ 		__webpack_require__.h = function() { return "06cbcec7603ebc4f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
